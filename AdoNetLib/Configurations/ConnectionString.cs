93a97dbe57e2cfd69da57e4436675ba64652c986
Игорь Павленко <illusiopard@gmail.com>
2023-06-20 23:05:20 +0300

    server

--- AdoNetLib/Configurations/ConnectionString.cs
+++ AdoNetLib/Configurations/ConnectionString.cs
@@ -9,6 +9,6 @@ namespace AdoNetLib.Configurations
     public static class ConnectionString
     {
         public static string MsSqlConnection =>
-            @"Server=DESKTOP-G5TER77.\SQLEXPRESS;Database=myBD;Trusted_Connection=True;";
+            @"Server=.\SQLEXPRESS;Database=myBD;Trusted_Connection=True;";
     }
 }
